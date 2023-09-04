2c99c17766d99d89e6c6cfc96e06ab9363cf53b3
henrynguyen13 <khoa.nd1301@gmail.com>
2023-09-04 16:46:12 +0700

    change inline css

--- src/app.js
+++ src/app.js
@@ -6,13 +6,11 @@ const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const viewsPath = path.join(__dirname, "../views");
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
-hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
